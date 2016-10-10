dd2ba2d86eb4b7c63c3598edb40033d943ea4b66
Berglind D. Sigurdardottir <berglinds14@ru.is>
2016-10-10 14:32:19 +0000

    Add files via upload

---
+++ iHeartMatlab.m
@@ -0,0 +1,13 @@
+%22.10.2014
+clear all; clc;
+
+t = -10:0.1:10;
+x = 16*sin(t).^3;
+y = 13*cos(t)-5*cos(2*t)-2*cos(3*t)-cos(4*t);
+
+plot(x,y,'r')
+axis([-20 20 -25 25]);
+
+text(-2,16,'I','FontSize',30); 
+text(-4,-20,'Matlab','FontSize',30);
+
