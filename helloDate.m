353439681a4fff3dd3ba2a63d6b38fb1f9a32895
Berglind D. Sigurdardottir <berglinds14@ru.is>
2016-10-10 13:05:01 +0000

    Add files via upload

---
+++ helloDate.m
@@ -0,0 +1,25 @@
+%04.10.14
+% Input: A string with a name.
+% Output: Hello with tha date and time.
+
+clear all; clc;
+
+name = input("Write your name: ", 's');
+hour = datestr(now,'HH');
+hour = str2num(hour);
+if (hour >= 6) && (hour < 12)
+    hello = 'Good morning ';
+elseif hour >= 18
+    hello = 'Good afternoon ';
+else
+    hello = 'Good evening ';
+end
+
+date = datestr(now,'dd.mmmm');
+year = datestr(now,'yyyy');
+time = datestr(now,'HH.MM.SS.');
+
+fprintf('%s %s. Today is %s %s. The time is %s \n', hello, name, date, year, time)
+
+end
+
